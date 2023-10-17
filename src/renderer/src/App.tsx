7e11a564295a299f002fbe8b298c732141557a2c
soonbro <tnsgud1110@gmail.com>
2023-10-17 17:08:55 +0900

    :poop: It's my first time using tailwind, nextui

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,17 @@
 const App = () => {
   return (
     <div id="app">
-      <h1>Hellow World!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+          <img className="h-12 w-12" src="../../../resources/icon.png" alt="ChitChat Logo"></img>
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
+      <h1>Hello World!</h1>
       <h3>
         I'm{' '}
         <a target="_blank" href="https://github.com/soonbro" rel="noopener noreferrer">
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <main className="dark text-foreground bg-background"></main>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 )
